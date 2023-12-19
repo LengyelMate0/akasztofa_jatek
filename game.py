aa9cfbe53e4d0ab3b2df74b9bf66169d6201d564
Lengyel <lengyelmate22@gmail.com>
2023-12-19 14:19:26 +0100

    akasztófa

---
+++ game.py
@@ -0,0 +1,27 @@
+import random
+
+def randomWordGenerator() -> str:
+    wordsList = ["alma", "körte", "barack", "szilva", "narancs", "eper", "citrom", "mandula", "datolya", "eperfa"]
+    return random.choice(wordsList)
+
+wordTemplate = []
+
+word = randomWordGenerator()
+#print(word)
+#List comprehension ehelyett jobb lesz 
+for i in range(len(word)):
+    wordTemplate.append("_")
+print()    
+print(" ".join(wordTemplate))
+
+print("\nTaláld ki a gondolt szót! 10 lehetőséged van, hogy írj betűket.")
+tip = input("Az ön tippje:")
+
+if tip not in word:
+    print("Ez a betű nincs a megoldásban. Próbálj egy újat!")
+else:
+    for i in range(len(word)):
+        if word[i]==tip:
+            wordTemplate[i]=tip
+            
+print(" ".join(wordTemplate))
